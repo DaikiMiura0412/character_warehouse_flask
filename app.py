d855783f1b4ff439eef60d99eb689b3e23689dcd
DaikiMiura0412 <mcl.daiki.41219@gmail.com>
2023-02-26 23:46:53 +0900

    first trial for heroku

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template # 変更
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html') # 変更
